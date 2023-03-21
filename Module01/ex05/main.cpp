52bfb1449535eb085e517aa0ad574e6995f94b71
jiyun <jiyun@student.42seoul.kr>
2023-03-21 17:25:54 +0900

    while문 삭제

--- Module01/ex05/main.cpp
+++ Module01/ex05/main.cpp
@@ -6,12 +6,7 @@ int main() {
 
 	std::cout << "Enter one of these 4 complaint DEBUG, INFO, WARNING, ERROR\n"
 				 << "or DONE if you want to end" << std::endl;
-	while (getline(std::cin, level)) {
-		if (level == "DONE")
-			break ;
-		harl.complain(level);
-		std::cout << "\nEnter one of these 4 complaint DEBUG, INFO, WARNING, ERROR\n"
-				  << "or DONE if you want to end" << std::endl;
-	}
+	getline(std::cin, level);
+	harl.complain(level);
 	return (0);
 }

21c4abdc651429bb03bc6cd39f2e16ba3c8f6908
jiyun <jiyun@student.42seoul.kr>
2023-04-25 12:02:16 +0900

    size_t 터질 때 catch

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -3,5 +3,10 @@
 int main() {
 	std::string arr[5] = {"h", "e", "l", "l", "o"};
 
+//	try {
+//		::iter(arr, -1, ::print);
+//	} catch (std::exception& e) {
+//		std::cout << e.what() << std::endl;
+//	}
 	::iter(arr, 5, ::print);
 }
